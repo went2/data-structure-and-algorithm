eedc54d8c9303dccbba3fa84a6ac89c4ac5d134d
wentao <569845499@qq.com>
2022-07-13 16:17:04 +0800

    complementation of a queue with linked list

---
+++ 4_queues/queue-linked-list-version.cpp
@@ -0,0 +1,24 @@
+//
+//  queue-linked-list-version.cpp
+//  
+//
+//  Created by WenTommy on 2022/7/13.
+//
+
+#include "queue-linked-list-version.hpp"
+
+void Enqueue(int x);
+int Dequeue(void);
+bool IsEmpty(void);
+int Front(void);
+void printQ(void);
+
+int main() {
+    Enqueue(11);Enqueue(22);Enqueue(33);Enqueue(44);
+    printQ();
+    Dequeue();Dequeue();Dequeue();Dequeue();
+    printf("isEmpty: %d: ", IsEmpty());
+    
+    return 0;
+    
+}
---
+++ 4_queues/queue-linked-list-version.hpp
@@ -0,0 +1,81 @@
+//
+//  queue-linked-list-version.hpp
+//  
+//  implemetation of a queue based on linked list
+//  Created by WenTommy on 2022/7/13.
+//
+
+#ifndef queue_linked_list_version_hpp
+#define queue_linked_list_version_hpp
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* queue_linked_list_version_hpp */
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// 全局变量 front, tear
+struct Node* front = NULL;
+struct Node* tear = NULL;
+
+void Enqueue(int x);
+int Dequeue(void);
+bool IsEmpty(void);
+int Front(void);
+void printQ(void);
+
+struct Node* getNewNode(int x);
+
+void Enqueue(int x) {
+    struct Node* temp = getNewNode(x);
+    if(front == NULL && tear == NULL) {
+        front = tear = temp;
+        return;
+    }
+    tear->next = temp;
+    tear = temp;
+}
+
+int Dequeue(void) {
+    struct Node* temp = front;
+    int value = temp->data;
+    
+    if(front == tear) {
+        front = tear = NULL;
+        free(temp);
+        return value;
+    }
+    front = temp->next;
+    free(temp);
+    return value;
+}
+
+bool IsEmpty(void) {
+    return (front == NULL) && (tear == NULL);
+}
+
+int Front(void) {
+    return front->data;
+}
+
+struct Node* getNewNode(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node*));
+    temp->data = x;
+    temp->next = NULL;
+    return temp;
+}
+
+void printQ(void) {
+    struct Node* temp = front;
+    
+    printf("\n===start printing queue===\n");
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n===printing finished===\n");
+}
