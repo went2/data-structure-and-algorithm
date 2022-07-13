9a1224f98fa5f70b102410c279db310e9b8600ab
wentao <569845499@qq.com>
2022-07-13 20:01:54 +0800

    增加链表的ts实现

--- 1_linked-list/js/singly-linked-list.ts
+++ 1_linked-list/js/singly-linked-list.ts
@@ -2,36 +2,46 @@
  * Implementation of singly linked list with JavsScript
  * 2022-07-13 By WenTommy
  */
-var LinkNode = /** @class */ (function () {
-    function LinkNode(data) {
+
+class LinkNode {
+    public data: number;
+    public next: LinkNode;
+
+    constructor(data: number) {
         this.data = data;
         this.next = null;
     }
-    return LinkNode;
-}());
-var SinglyLinkedList = /** @class */ (function () {
-    function SinglyLinkedList() {
+}
+
+class SinglyLinkedList {
+    public length: number;
+    public head: LinkNode;
+    public tail: LinkNode;
+
+    constructor() {
         this.length = 0;
         this.head = null;
         this.tail = null;
     }
+
     // 将一个节点插入链表尾部，作为最后一个节点
-    SinglyLinkedList.prototype.push = function (data) {
-        var newNode = new LinkNode(data);
+    push(data: number): void {
+        const newNode = new LinkNode(data);
+
         if (this.head === null) {
             this.head = newNode;
             this.tail = newNode;
-        }
-        else {
+        } else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
         this.increaseLength();
-    };
+    }
+
     // 在链表的头部插入一个节点
-    SinglyLinkedList.prototype.insertAtHead = function (data) {
-        var temp = new LinkNode(data);
-        if (this.head === null) {
+    insertAtHead(data: number): void {
+        const temp = new LinkNode(data);
+        if(this.head === null) {
             this.head = temp;
             this.increaseLength();
             return;
@@ -39,69 +49,77 @@ var SinglyLinkedList = /** @class */ (function () {
         temp.next = this.head;
         this.head = temp;
         this.increaseLength();
-    };
+    }
+
     // 链表索引从1开始，head 为第1个节点
-    SinglyLinkedList.prototype.insertAt = function (data, pos) {
-        var temp = new LinkNode(data);
-        if (this.head === null) {
+    insertAt(data: number, pos: number): void {
+        const temp = new LinkNode(data);
+
+        if(this.head === null) {
             this.head = temp;
             this.increaseLength();
-        }
-        else if (pos > this.length) {
+        } else if(pos > this.length) {
             this.push(data);
-        }
-        else {
-            var temp2 = this.head;
-            for (var i = 0; i < pos - 1; i++) {
+        } else {
+            let temp2: LinkNode = this.head;
+            for(let i=0; i<pos-1;i++) {
                 temp2 = temp2.next;
             }
             temp.next = temp2.next;
             temp2.next = temp;
             this.increaseLength();
         }
-    };
-    SinglyLinkedList.prototype.deleteNode = function (pos) {
-        if (this.head === null) {
+    }
+
+    deleteNode(pos: number): void {
+        if(pos < 1) {
+            console.log('DeleteError: 位置不能小于1');
+            return;
+        }
+        if(this.head === null) {
             console.log('Error: empty linked list');
             return;
         }
-        if (pos > this.length) {
+        if(pos > this.length) {
             console.log('Error: 超过链表长度');
             return;
         }
-        if (this.length === 1) {
+        if(this.length === 1) {
             this.head = null;
             this.tail = null;
             this.length = 0;
             return;
         }
         // 找到 pos-1 号的node
-        var temp = this.head;
-        for (var i = 0; i < pos - 1; i++) {
+        let temp: LinkNode = this.head;
+        for(let i=0; i<pos-2; i++) {
             temp = temp.next;
         }
+
         temp.next = temp.next.next;
         this.decreaseLength();
-    };
-    SinglyLinkedList.prototype.printLL = function () {
-        if (this.head === null) {
+    }
+
+    printLL(): void {
+        if(this.head === null) {
             console.log('empty linked list');
             return;
         }
-        var temp = this.head;
-        while (temp !== null) {
-            temp = temp.next;
+        let temp: LinkNode = this.head;
+        while(temp !== null) {
             console.log(temp.data);
+            temp = temp.next;
         }
-    };
+    }
+
     // increasing length of linked list by 1
-    SinglyLinkedList.prototype.increaseLength = function () {
+    increaseLength() {
         this.length++;
-    };
-    SinglyLinkedList.prototype.decreaseLength = function () {
-        if (this.length <= 0)
-            return;
+    }
+    decreaseLength() {
+        if(this.length <= 0) return;
         this.length--;
-    };
-    return SinglyLinkedList;
-}());
+    }
+
+}
+
