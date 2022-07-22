14d07eea4084c1afb63f965c6eff681f93ecdef9
ywt <569845499@qq.com>
2022-07-22 13:59:39 +0800

    use object to construct a frequency counter

--- Algorithms/frequency-counter/frequencyCounter.js
+++ Algorithms/frequency-counter/frequencyCounter.js
@@ -10,21 +10,43 @@
 
 // first try: find and delete
 // 遍历数组1，查找数组2中是否有对应的平方值，有则移除，遍历结束返回true。
-function same(arr1, arr2) {
-  const l1 = arr1.length, l2 = arr2.length;
-  if (l1 !== l2) return false;
+// 嵌套循环 O(n^2)
+function same1(arr1, arr2) {
+	const l1 = arr1.length, l2 = arr2.length;
+	if (l1 !== l2) return false;
 
-  for (let i = 0; i < l1; i++) {
-    let index2 = arr2.findIndex(e => e === arr1[i] ** 2);
+	for (let i = 0; i < l1; i++) {
+		let index2 = arr2.findIndex(e => e === arr1[i] ** 2);
 
-    if (index2 !== -1) {
-      arr2.splice(index2, 1);
-    }
-    else return false;
-  }
-  return true;
+		if (index2 !== -1) {
+			arr2.splice(index2, 1);
+		}
+		else return false;
+	}
+	return true;
 }
 
-console.log(same([1, 2, 3], [4, 1, 9]));
-console.log(same([1, 2, 3], [1, 9]));
-console.log(same([1, 2, 1], [4, 4, 1]));
+// 使用 object
+// 多个独立的 loop 比多个嵌套的 loop 好，前者按加法算复杂度，后者按乘法算
+function same2(arr1, arr2) {
+	if (arr1.length !== arr2.length) return false;
+	let frequencyCounter1 = {};
+	let frequencyCounter2 = {};
+
+	for (let value of arr1) {
+		frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1;
+	}
+	for (let value of arr2) {
+		frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1;
+	}
+
+	for (let key in frequencyCounter1) {
+		if (!(key ** 2 in frequencyCounter2)) return false;
+		if (frequencyCounter1[key] !== frequencyCounter2[key ** 2]) return false;
+	}
+	return true;
+}
+
+console.log(same2([1, 2, 3], [4, 1, 9]));
+console.log(same2([1, 2, 3], [1, 9]));
+console.log(same2([1, 2, 1], [4, 4, 1]));
