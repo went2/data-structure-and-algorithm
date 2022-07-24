6c1b4a7244db005059e4159c028ab6505fb706b2
ywt <569845499@qq.com>
2022-07-25 00:20:48 +0800

    complete mergerSort

--- Algorithms/4_intermediate-sort/merge.js
+++ Algorithms/4_intermediate-sort/merge.js
@@ -1,7 +1,15 @@
 /**
- * function mergeSort(arr1: number[], arr2: number[]): number[]
+ * function mergeSort(arr: number[]): number[]
  * 
  */
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
 
 // 输入两个已排序的数组，合并它们，返回排序后的数组
 // 要求时间复杂度O(n+m), 空间复杂度O(n+m),即每个item在每个数组中只被遍历一次(没有嵌套遍历)
@@ -22,18 +30,13 @@ function merge(arr1, arr2) {
             j++;
         }
     }
-
-    console.log('i:', i, 'j:', j)
-    console.log(newArr);
     // 说明 arr2 已被合并，arr1 还有元素
-    if (i < len1 - 1)
-        newArr.push(arr1.slice(i));
-    else if (j < len2 - 1)
-        newArr.push(...arr2.slice(j));
-
-    console.log(newArr);
+    if (i < len1) newArr.push(...arr1.slice(i));
+    else if (j < len2) newArr.push(...arr2.slice(j));
     return newArr;
 }
 
 // merge([-2, 0, 2, 5, 7, 21], [3, 5, 8, 9, 44, 67]);
-merge([-2, 0, 2, 5, 7, 21], [3, 5, 8, 9, 44, 67, 89]);
+// merge([-2, 0, 2, 5, 7, 21], [3, 5, 8, 9, 44, 67, 89]);
+const sorted = mergeSort([44, 3, 89, 0, 5, 8, -2, -4, 9, 67, -34]);
+console.log(sorted);
